687d8a3573257a5eaacc02a288a39e282c4c45b5
ironreplica <trevorchilds8@gmail.com>
2024-04-15 10:46:29 -0600

    Fixed render site

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
 const myController = require("../controllers");
 const routes = require("express").Router();
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("../swagger.json");
+
 
 routes.get("/", myController.awesomeFunction);
 
 routes.get("/ttech", myController.tooeleTech);
 
 routes.use("/students", require("./students"));
-routes.use("/api-docs", swaggerUi.serve);
-routes.use("/api-docs", swaggerUi.setup(swaggerDocument));
 
+routes.use("/api-docs", require("./swagger"));
 
 module.exports = routes;
