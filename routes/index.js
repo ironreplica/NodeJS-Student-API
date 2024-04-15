a39e6b054153ff762d665c3fd1778c288f4545bc
ironreplica <trevorchilds8@gmail.com>
2024-04-15 10:16:12 -0600

    Swagger API

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const myController = require("../controllers");
 const routes = require("express").Router();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
 
 routes.get("/", myController.awesomeFunction);
 
 routes.get("/ttech", myController.tooeleTech);
 
 routes.use("/students", require("./students"));
+routes.use("/api-docs", swaggerUi.serve);
+routes.use("/api-docs", swaggerUi.setup(swaggerDocument));
+
 
 module.exports = routes;
---
+++ swagger.js
@@ -0,0 +1,22 @@
+const swaggerAutogen = require("swagger-autogen");
+
+const doc = {
+    info: {
+        title: "My student API",
+        description: "Api for showing students data.",
+    },
+    host: "node-routes-xs4z.onrenderer.com",
+    // host: "localhost:3000",
+    schemes: ["http"],
+};
+
+const outputfile = "./swagger.json";
+const endpointFiles = ["./routes/index.js"];
+
+// Run to
+swaggerAutogen(outputfile, endpointFiles, doc);
+
+// Generates the swagger.json file
+swaggerAutogen(outputfile, endpointFiles, doc).then(async() => {
+    await import("./server.js");
+});
